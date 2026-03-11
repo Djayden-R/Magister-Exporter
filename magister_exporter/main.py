f037a2595b74048a0fa3926b849f79afe311a47d
Djayden-R <6953@rgomiddelharnis.nl>
2026-03-11 23:07:30 +0100

    make program a loop and add uuid option and block listing directory

--- magister_exporter/main.py
+++ magister_exporter/main.py
@@ -14,6 +14,11 @@ PROGRAM_PATH = Path("/usr/src/app")
 OPTIONS_FILE_PATH = Path("/data/options.json")
 CALENDAR_FOLDER = PROGRAM_PATH / "calendars"
 
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(("8.8.8.8", 80))
+IP_ADRESS = s.getsockname()[0]
+s.close()
+
 
 # Source - https://stackoverflow.com/a/52531444
 # Posted by Andy Hayden, modified by community. See post 'Timeline' for change history
@@ -22,6 +27,11 @@ CALENDAR_FOLDER = PROGRAM_PATH / "calendars"
 class HTTPHandler(SimpleHTTPRequestHandler):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, directory=CALENDAR_FOLDER, **kwargs)
+    
+    # Prevent scrapers from finding names of calendars
+    def list_directory(self, path):
+        self.send_error(404)
+        return None
 
 
 def get_options():
@@ -32,8 +42,9 @@ def get_options():
     # Expected format: [{'username': username, 'password': password}, ...]
     credentials_list: list[dict] = options['credentials']
     days_to_fetch: int = options['days_to_fetch']
+    refresh_time: int = options['refresh_time']
 
-    return credentials_list, days_to_fetch
+    return credentials_list, days_to_fetch, refresh_time
 
 
 def get_user_info(username):
@@ -85,7 +96,7 @@ def start_http_server():
     thread = threading.Thread(target=server.serve_forever)
     thread.daemon = True
     thread.start()
-    print(f"Started server on port 15060")
+    print(f"Started server on http://{IP_ADRESS}:15060")
 
 
 async def main():
@@ -94,48 +105,55 @@ async def main():
         print(f"Created folder: {CALENDAR_FOLDER}")
     
     start_http_server()
-    credentials_list, days_to_fetch = get_options()
+    credentials_list, days_to_fetch, refresh_time = get_options()
 
     if not credentials_list:
         logging.warning("Credentials not defined, exiting program...")
         return
 
-    for credenials in credentials_list:
-        name = credenials.get('name', None)
-        username = credenials.get('username', None)
-        password = credenials.get('password', None)
+    while True:
+        for credenials in credentials_list:
+            name = credenials.get('name', None)
+            username = credenials.get('username', None)
+            password = credenials.get('password', None)
+            uuid = credenials.get('uuid', None)
 
-        print(f"Checking info for {username}")
+            print(f"Checking info for {username}")
 
-        if not (username and password):
-            logging.error(f"Invalid credentials found (username={username}, password={password})")
-            continue
+            if not (username and password):
+                logging.error(f"Invalid credentials found (username={username}, password={password})")
+                continue
+            
+            token, user_id = get_user_info(username)
+
+            calendar = None
+
+            if token and user_id:
+                print("Token found in tokens.json")
+                calendar = fetch_magister_calendar(user_id, token, days_to_fetch)
         
-        token, user_id = get_user_info(username)
-
-        calendar = None
-
-        if token and user_id:
-            print("Token found in tokens.json")
+            if not (token and user_id) or not calendar:
+                print("Fetching token...")
+                async with async_playwright() as playwright:
+                    token, user_id = await fetch_magister_token(playwright, name, username, password)
+            
+            save_user_info(username, token, user_id)
+            
             calendar = fetch_magister_calendar(user_id, token, days_to_fetch)
-    
-        if not (token and user_id) or not calendar:
-            print("Fetching token...")
-            async with async_playwright() as playwright:
-                token, user_id = await fetch_magister_token(playwright, name, username, password)
-        
-        save_user_info(username, token, user_id)
-        
-        calendar = fetch_magister_calendar(user_id, token, days_to_fetch)
 
-        if not calendar:
-            print("Unable to fetch magister calendar")
-            continue
+            if not calendar:
+                print("Unable to fetch magister calendar")
+                continue
 
-        ics_calendar = calendar_to_ics(calendar)
+            ics_calendar = calendar_to_ics(calendar)
 
-        save_ics_file(ics_calendar, CALENDAR_FOLDER, "Djayden_Magister.ics")
-    await asyncio.sleep(50000)
+            file_name = f"{uuid}.ics"
+
+            save_ics_file(ics_calendar, CALENDAR_FOLDER, file_name)
+
+            print(f"{name}'s calendar is hosted on http://{IP_ADRESS}:15060/{file_name}")
+
+        await asyncio.sleep(refresh_time * 60)
 
 if __name__ == "__main__":
     asyncio.run(main())
